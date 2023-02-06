877390eba4f0cb067b9eb09e8e87e26f7583df43
Mouzzli <cokes03@yandex.ru>
2023-02-06 17:45:54 +0300

    2. Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/001_dll_create_table_auto_user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE auto_user
+(
+    id       serial primary key,
+    login    varchar unique not null,
+    password varchar        not null
+)
+
+
---
+++ db/scripts/002_ddl_create_table_auto_post.sql
@@ -0,0 +1,8 @@
+CREATE TABLE auto_post
+(
+    id           serial primary key,
+    description  varchar                       not null,
+    created      timestamp                     not null,
+    auto_user_id int references auto_user (id) not null
+
+);
